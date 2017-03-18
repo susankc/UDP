6bac373d2af967bdb6195a732ce440db5bb4c755
Susan K.C <susan.kc13@gmail.com>
2017-03-18 23:21:27 +0545

    Adding the acknwlodge and synchronization file

--- fileHandler.java
+++ fileHandler.java
@@ -1,60 +1,56 @@
-class  fileHandler
-{
+import java.util.Arrays;
+import java.io.Serializable;
+import java.io.*;
 
-static String filename;
-   
-        
-        int MAX_SIZE = 52428800;
+public class Packet implements Serializable {
+	private boolean isAck;
+	private boolean isPayLoad;
+	private boolean isSync;
 
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IpAddress = InetAddress.getByName("localhost");
+	public Packet(boolean ack, boolean payL, boolean sync){
+		isAck = ack;
+		isPayLoad = payL;
+		isSync = sync;
+	}
 
-        byte[] sendData = new byte[MAX_SIZE];
+	public boolean getIsAck () {
+		return isAck;
+	}
 
-        String filePath = "D:\\hello.mp4";
-        File file = new File(filePath);
-        FileInputStream fis = new FileInputStream(file);
+	public boolean getIsPayLoad () {
+		return isPayLoad;
+	}
 
+	public boolean getIsSync () {
+		return isSync;
+	}
+	
+	public void SerializePacket (String fileName) {
+		try{
+			FileOutputStream fileOut = new FileOutputStream(fileName);
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(this);
+			out.close();
+			fileOut.close();
+			System.out.printf("Serialized data is saved as " + fileName);
+		}catch(IOException i){
+			i.printStackTrace();
+		}
+	}
 
-        int noOfPackets = totLength/MAX_SIZE;
-        System.out.println("No of packets : " + noOfPackets);
+	public void DeSerializePacket (String fileName){
+		try{
+			FileInputStream fileIn = new FileInputStream(fileName);
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			this = (Packet) in.readObject();
+			in.close();
+			fileIn.close();
+		}catch(IOException i){
+			i.printStackTrace();
+		}catch(ClassNotFoundException c){
+			System.out.println("Student class not found");
+			c.printStackTrace();
+		}
+	}
 
-        int off = noOfPackets * MAX_SIZE;  //calculate offset. it total length of file is 52428800 and array size is 1000 den starting position of last packet is 1001. this value is stored in off.
-
-        int lastPackLen = totLength - off;
-        System.out.println("\nLast packet Length : " + lastPackLen);
-
-        byte[] lastPack = new byte[lastPackLen-1];  //create new array without redundant information
-
-
-        fis.close();
-
-        FileInputStream fis1 = new FileInputStream(file);
-        //while((count = fis1.read(sendData)) != -1 && (noOfPackets!=0))
-        while((count = fis1.read(sendData)) != -1 )
-        { 
-            if(noOfPackets<=0)
-                break;
-            System.out.println(new String(sendData));
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IpAddress, 9876);
-            clientSocket.send(sendPacket);
-            System.out.println("========");
-            System.out.println("last pack sent" + sendPacket);
-        noOfPackets--;
-        }
-
-        //check
-        System.out.println("\nlast packet\n");
-        System.out.println(new String(sendData));
-
-        lastPack = Arrays.copyOf(sendData, lastPackLen);
-
-        System.out.println("\nActual last packet\n");
-        System.out.println(new String(lastPack));
-                //send the correct packet now. but this packet is not being send. 
-        DatagramPacket sendPacket1 = new DatagramPacket(lastPack, lastPack.length, IpAddress, 98521);
-        clientSocket.send(sendPacket1);
-        System.out.println("last pack sent" + sendPacket1);
-
-    
 }
